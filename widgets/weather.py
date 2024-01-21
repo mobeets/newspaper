1d6a0cbe5a1e59387f33527dbc5d7d439615cb0d
Jay H <mobeets@gmail.com>
2024-01-21 16:20:03 -0500

    comic, maze, sudoku

--- widgets/weather.py
+++ widgets/weather.py
@@ -4,22 +4,26 @@ import matplotlib.pyplot as plt
 import requests
 from datetime import datetime
 
+import os.path
+import pathlib
+CURDIR = pathlib.Path(__file__).parent.resolve()
+
 import matplotlib as mpl
-mpl.rcParams['font.size'] = 10
+mpl.rcParams['font.size'] = 14
 mpl.rcParams['figure.figsize'] = [3.0, 3.0]
 mpl.rcParams['figure.dpi'] = 300
 plt.rcParams['font.family'] = 'Helvetica'
 # mpl.rcParams['axes.spines.right'] = False
 mpl.rcParams['axes.spines.top'] = False
 
+CACHE_PATH = os.path.join(CURDIR, 'cache/forecast.json')
 BASE_URL = 'https://api.weather.gov/points/{lat},{lon}'
-
 locs = {'Somerville, MA': [42.39, -71.0868]}
 
 def dtstr_to_dt(dtstr):
 	return datetime.strptime(dtstr[:13], '%Y-%M-%dT%H')
 
-def plot(rows, outfile='out.png'):
+def plot(rows, outfile=None):
 	plt.figure(figsize=(4,2))
 	xs = np.array([row['dt'].hour for row in rows])
 	ys1 = np.array([row['temp'] for row in rows])
@@ -38,8 +42,10 @@ def plot(rows, outfile='out.png'):
 	ax2.set_ylabel('precip. (%)', color=color)
 	ax2.set_ylim([-2, 100])
 	plt.tight_layout()
+	plt.title("Today's forecast")
 
-	plt.savefig(outfile)
+	if outfile is not None:
+		plt.savefig(outfile)
 	plt.close()
 
 def is_same_day(dt1, dt2):
@@ -57,9 +63,9 @@ def get_time_series(out):
 	return rows
 
 def load_cached():
-	return json.load(open('out.json'))
+	return json.load(open(CACHE_PATH))
 
-def main(city, base_url=BASE_URL, cached=True):
+def main(city, base_url=BASE_URL, cached=True, outfile=None):
 	if cached:
 		out = load_cached()
 	else:
@@ -69,9 +75,9 @@ def main(city, base_url=BASE_URL, cached=True):
 		forecast_url = out['properties']['forecastHourly']
 		response = requests.get(forecast_url)
 		out = response.json()
-		json.dump(out, open('out.json', 'w'))
+		json.dump(out, open(CACHE_PATH, 'w'))
 	rows = get_time_series(out)
-	plot(rows)
+	plot(rows, outfile=outfile)
 
 if __name__ == '__main__':
-	main(city='Somerville, MA')
+	main(city='Somerville, MA', cached=True, outfile=os.path.join(CURDIR, 'cache', 'forecast.png'))
