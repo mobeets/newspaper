1cbd9857808ae0739e6586a9438bc5ecbd2f663e
Jay H <mobeets@gmail.com>
2024-01-22 10:10:20 -0500

    rendering

--- newspaper.py
+++ newspaper.py
@@ -9,9 +9,13 @@ TEMPLATE_DIR = os.path.join(CURDIR, 'tex')
 RENDERED_DIR = os.path.join(CURDIR, 'rendered')
 TEMPLATE_NAME = 'newspaper.tex'
 
-def make_new_folder():
+def get_paths():
 	dirname = datetime.now().strftime('%Y%m%d')
 	dirname = os.path.join(RENDERED_DIR, dirname)
+	texpath = os.path.join(dirname, TEMPLATE_NAME)
+	return dirname, texpath
+
+def make_new_folder(dirname):
 	shutil.copytree(TEMPLATE_DIR, dirname, dirs_exist_ok=True)
 	return dirname
 
@@ -26,9 +30,11 @@ def build_tex(dirname, texpath):
 	subprocess.check_call(['pdflatex', '-halt-on-error', '-output-directory', dirname, texpath])
 
 def main():
-	# make new directory containing newspaper.tex and images folder
-	dirname = make_new_folder()
-	texpath = os.path.join(dirname, TEMPLATE_NAME)
+	# get paths we will use for today's paper
+	dirname, texpath = get_paths()
+
+	# copy template directory to our new directory
+	dirname = make_new_folder(dirname)
 
 	# run each widget (write images to images/ in new dir)
 	results = run_widgets(dirname)
