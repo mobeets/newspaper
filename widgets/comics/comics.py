7b5cb14cf55cd7520278980d0df56324080848c8
Jay H <mobeets@gmail.com>
2024-01-24 15:39:28 -0500

    calvinandhobbes

--- widgets/comics/comics.py
+++ widgets/comics/comics.py
@@ -5,6 +5,7 @@ import requests
 import subprocess
 import pandas as pd
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 CURDIR = pathlib.Path(__file__).parent.resolve()
 CACHE_DIR = os.path.abspath(os.path.join(CURDIR, '..', 'cache'))
@@ -12,7 +13,8 @@ CACHE_PATH = os.path.join(CACHE_DIR, 'comic-{}.html')
 
 BASE_URLS = {
 	'xkcd': 'https://xkcd.com/',
-	'pearls': 'https://www.gocomics.com/pearlsbeforeswine/2024/01/24'
+	'calvinandhobbes': 'https://www.gocomics.com/calvinandhobbes/{dtstr}',
+	'pearls': 'https://www.gocomics.com/pearlsbeforeswine/{dtstr}'
 	}
 
 def get_comic(soup, name):
@@ -22,11 +24,16 @@ def get_comic(soup, name):
 		caption = img.attrs['title']
 		binary = requests.get(url).content
 		return {'url': url, 'binary': binary, 'caption': caption}
-	elif name == 'pearls':
+	elif name in ['pearls', 'calvinandhobbes']:
 		img = soup.select('.comic__image')[0].select('picture')[0].select('img')[0]
 		url = img.attrs['src']
 		binary = requests.get(url).content
 		return {'url': url, 'binary': binary, 'ext': '.gif'}
+	# elif name == 'farside':
+	# 	img = soup.select('.tfs-comic__image')[0].select('img')[0]
+	# 	url = img.attrs['data-src']
+	# 	binary = requests.get(url).content
+	# 	return {'url': url, 'binary': binary, 'ext': '.gif'}
 
 def render(item, outfile):
 	outfile += item.get('ext', '.png')
@@ -34,6 +41,9 @@ def render(item, outfile):
 		f.write(item['binary'])
 	if item.get('ext', '.gif'):
 		subprocess.check_call(['convert', outfile, outfile.replace('.gif', '.png')])
+
+def is_cached(name, cache_path=CACHE_PATH):
+	return os.path.exists(cache_path.format(name))
 
 def load_cached(name, cache_path=CACHE_PATH):
 	return json.load(open(cache_path.format(name)))
@@ -46,13 +56,16 @@ def fetch_comic(name, base_url, cache_path=CACHE_PATH):
 	return content
 
 def main(name, outdir=CACHE_DIR, cached=True):
-	if cached:
+	if cached and is_cached(name):
 		content = load_cached(name)
 	else:
-		content = fetch_comic(name, BASE_URLS[name])
+		comic_url = BASE_URLS[name]
+		if '{dtstr}' in comic_url:
+			comic_url = comic_url.format(dtstr=datetime.today().strftime('%Y/%m/%d'))
+		content = fetch_comic(name, comic_url)
 	soup = BeautifulSoup(content, features="lxml")
 	item = get_comic(soup, name)
 	render(item, os.path.join(outdir, 'comic-' + name))
 
 if __name__ == '__main__':
-	main('pearls', cached=True)
+	main('calvinandhobbes', cached=True)
